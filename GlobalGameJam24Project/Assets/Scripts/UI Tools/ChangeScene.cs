779c9681ce7fddf990b6cbbfccc726c22c4d43f2
Danica Mobley <dcmobley@ualr.edu>
2024-01-28 10:12:44 -0600

    Minor changes to start screen

--- GlobalGameJam24Project/Assets/Scripts/UI Tools/ChangeScene.cs
+++ GlobalGameJam24Project/Assets/Scripts/UI Tools/ChangeScene.cs
@@ -5,7 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public float delay = 0f;
+
     public void LoadScene(string sceneName) {
+        StartCoroutine(SwapSceneWithDelay(sceneName));
+    }
+
+    private IEnumerator SwapSceneWithDelay(string sceneName)
+    {
+        yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(sceneName);
     }
 }
